0ffb4eab316a80c9f2bb9e91b5ef5016f7e994f1
Sempre <141326593+Sempre0721@users.noreply.github.com>
2024-04-14 15:06:31 +0800

    Update 摄像头人形检测.py

--- 摄像头人形检测.py
+++ 摄像头人形检测.py
@@ -3,15 +3,15 @@ from ultralytics import YOLO
 model = YOLO('yolov8n-pose.pt')
 cap = cv2.VideoCapture(0) 
 while cap.isOpened():
-    success, frame = cap.read()
+    success, frame_init = cap.read()
+    frame = cv2.flip(frame_init, 1)
     if success:
         results = model(frame)
         annotated_frame = results[0].plot()
-        cv2.imshow("YOLOv8 Inference", annotated_frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+        cv2.imshow('YOLOv8 Inference',annotated_frame)
+        if cv2.waitKey(1) & 0xFF == 27:
             break
     else:
         break
 cap.release()
-if cv2.waitKey(1) & 0xff == self.Exit_key:
-    cv2.destroyAllWindows()
+cv2.destroyAllWindows()
